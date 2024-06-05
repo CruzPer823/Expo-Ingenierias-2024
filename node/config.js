a291e12ddd4fb177c0eed3283d443a27d8ba4681
mikeedel <105459492+mikeedel@users.noreply.github.com>
2024-06-05 15:53:28 -0600

    credenciales smtp

--- node/config.js
+++ node/config.js
@@ -7,12 +7,17 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID
 const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET
+const USER = process.env.USER
+const MAIL = process.env.MAIL
+const PASS = process.env.PASS
 
 export default { 
     DB_CONNECTION_STRING,
     DB_PASSWORD,
     AUTH0_DOMAIN,
     AUTH0_CLIENT_ID,
-    AUTH0_CLIENT_SECRET
+    AUTH0_CLIENT_SECRET,
+    USER,
+    MAIL,
+    PASS
 };
-
