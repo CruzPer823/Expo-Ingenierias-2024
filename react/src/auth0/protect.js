a959f5338086225e6302878961ee01d28d388b79
mikeedel <105459492+mikeedel@users.noreply.github.com>
2024-06-12 21:17:39 -0600

    Update protect.js

--- react/src/auth0/protect.js
+++ react/src/auth0/protect.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, requiredRole }) => {
   const { isAuthenticated, isLoading } = useAuth0();
+  const navigate = useNavigate();
   const userRole = localStorage.getItem('userRole');
 
   if (isLoading) {
